f6fa9ac2a3d90941050841b9d81c3b425c21ffe9
Mike McQuaid <mike@mikemcquaid.com>
2017-03-26 16:38:12 +0100

    deleteclone.sh: validate VMNAME.

--- deleteclone.sh
+++ deleteclone.sh
@@ -8,6 +8,12 @@ then
 fi
 readonly VMNAME=$1
 
+if echo "$VMNAME" | grep "/"
+then   
+  echo '$VMNAME cannot contain slashes!'
+  exit 1
+fi
+
 VMID=$(vim-cmd vmsvc/getallvms | awk "/^[0-9]+ +$VMNAME /{print \$1}")
 vim-cmd vmsvc/power.off $VMID
 vim-cmd vmsvc/unregister $VMID
